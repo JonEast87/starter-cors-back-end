65ec1b6bf647ed034cd609918f4327d54f0688e1
Jon Eastin <picardncce@gmail.com>
2022-07-28 09:44:28 -0700

    finished to spec spec for 37.3

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,18 +1,26 @@
-const router = require("express").Router();
-const controller = require("./cors-enabled.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+const router = require('express').Router()
+const controller = require('./cors-enabled.controller')
+const methodNotAllowed = require('../errors/methodNotAllowed')
+const cors = require('cors')
+
+// const corsDelete = cors({ methods: 'DELETE ' }) // CORS just for the delete button
+
+// router.use(cors()) // adds CORS to the entire router
 
 router
-  .route("/:corsId")
-  .get(controller.read)
-  .put(controller.update)
-  .delete(controller.delete)
-  .all(methodNotAllowed);
+	.route('/:corsId')
+	// .all(cors())
+	.get(controller.read)
+	.put(controller.update)
+	// .delete(corsDelete, controller.delete) // just CORS for delete
+	// .options(corsDelete)  // CORS is enabled for routes that have OPTIONS
+	.delete(controller.delete)
+	.all(methodNotAllowed)
 
 router
-  .route("/")
-  .get(controller.list)
-  .post(controller.create)
-  .all(methodNotAllowed);
+	.route('/')
+	.get(cors(), controller.list)
+	.post(controller.create)
+	.all(methodNotAllowed)
 
-module.exports = router;
+module.exports = router
